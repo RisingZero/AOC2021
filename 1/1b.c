4c08d0d8680e895d9b2900b3b8afe4388cd97147
RisingZero <ar.angelo259@gmail.com>
2021-12-01 14:08:17 +0100

    1b completed

--- 1/1b.c
+++ 1/1b.c
@@ -4,16 +4,18 @@ int main(void) {
   FILE *fp;
   int numIncreasing = 0;
   int vRead;
-  int value, prev, prevprev, i = 0;
+  int value, prev, prevprev, prevprevprev, i = 0;
 
   fp = fopen("input.txt", "r");
   while (!feof(fp) && (fscanf(fp, "%d", &value) == 1)) {
-    if (i > 0) {
-      if (value > prev)
+    if (i > 2) {
+      if (value+prev+prevprev > prev+prevprev+prevprevprev)
         numIncreasing++;
     }
 
     i++;
+    prevprevprev = prevprev;
+    prevprev = prev;
     prev = value;
   }
 
